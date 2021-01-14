c94b5267c4c8ce11b18558749b13abda68fb8cfb
rawxbee <69788946+rawxbee@users.noreply.github.com>
2021-01-14 06:06:49 -0330

    edited desc

--- dailies.js
+++ dailies.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Dailies
-// @author       rawbeee
+// @author       rawbeee - including edits from sunbathr
 // @match        http://www.neopets.com/*
 // @grant        none
 // ==/UserScript==
