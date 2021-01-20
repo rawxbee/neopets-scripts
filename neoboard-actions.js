efe5bc4f1c658a751161fd401011965f486864ec
rawxbee <69788946+rawxbee@users.noreply.github.com>
2021-01-20 00:22:13 -0330

    @match update

--- neoboard-actions.js
+++ neoboard-actions.js
@@ -4,7 +4,7 @@
 // @version      1.3
 // @description  Adds buttons to each post that allows you to respond to the specific user, mail the specific user, view the specific user's auctions/trades/shop and refresh the thread. The script will also auto-select your last used pen.
 // @author       rawbeee - code borrowed/edited from sunbath (https://github.com/moonbathr/neopets/tree/main)
-// @match        http://www.neopets.com/neoboards/*
+// @match        http://www.neopets.com/neoboards/topic*
 // @require      http://code.jquery.com/jquery-latest.js
 // @require      http://userscripts-mirror.org/scripts/source/107941.user.js
 // @grant        GM_setValue
